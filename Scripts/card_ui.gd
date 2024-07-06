4ce8e00c18fd96c9a04b832f448d72a1bc20ab02
Joey Romanowski <javachup@gmail.com>
2024-07-06 19:29:54 -0400

    adds size changing on toggle

--- Scripts/card_ui.gd
+++ Scripts/card_ui.gd
@@ -2,9 +2,14 @@ extends TextureButton
 class_name UICard
 
 const TEXTURE_PATH = "res://Art/Playing Cards/"
+const SIZE_INCREASE = 50.0
+const SIZE_LERP_SPEED = 0.1
 
 var suit:Card.Suit = Card.Suit.BACK
 var rank:int # 0=Ace, 12=King
+
+var original_size:int
+var target_size:float
 
 func set_card(suit:Card.Suit, rank:int):
 	self.suit = suit
@@ -19,5 +24,15 @@ func set_card(suit:Card.Suit, rank:int):
 	texture_hover = texture
 	texture_pressed = texture
 
+func _ready():
+	original_size = custom_minimum_size.x
+	target_size = original_size
+
 func _on_toggled(toggled_on):
-	pass # Replace with function body.
+	if toggled_on:
+		target_size = original_size + SIZE_INCREASE
+	else:
+		target_size = original_size
+
+func _process(_delta):
+	custom_minimum_size.x = lerp(custom_minimum_size.x, target_size, SIZE_LERP_SPEED)
