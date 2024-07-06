4e373175a7328e423ff38d7788d0824440b64670
Joseph Lyons <joseph.lyons@mymail.champlain.edu>
2024-07-06 02:28:50 -0400

    Added some juice to the hopping

--- Scenes/Scarecrow.gd
+++ Scenes/Scarecrow.gd
@@ -2,18 +2,35 @@ extends RigidBody2D
 
 
 @export var bottom: RigidBody2D
+@export var sprite: Sprite2D
+@export var chargeCurve: Curve
+@export var returnToStandingCurve : Curve
 
 @export var jumpForceRange : Vector2
 @export var rotateSpeed: float
 @export var maxChargeTime : float
+@export var returnToStandingTime : float
+@export var chargeSpriteDistanceMult : float
 
 var onFloor : bool = false
 var charging : bool = false
 
+var distToBottom : float = 0
+
 var chargeTime : float = 0
+
+var standingReturnTimePassed : float = 1000
+
+var spriteLoggedDist : float = 0
+
+
+func _enter_tree() -> void:
+	distToBottom = (bottom.global_position - global_position).length()
 
 
 func _process(delta: float) -> void:
+	sprite.global_rotation = bottom.global_rotation
+	
 	if Input.is_action_pressed("left"):
 		apply_force(Vector2.LEFT * rotateSpeed)
 		
@@ -24,6 +41,11 @@ func _process(delta: float) -> void:
 		charging = true
 		chargeTime += delta
 		
+		#var vecToBottom = bottom.global_position - global_position
+		var dirToBottom = (bottom.global_position - global_position).normalized()
+		sprite.global_position = global_position.lerp(global_position + dirToBottom * distToBottom * chargeSpriteDistanceMult, chargeCurve.sample(clampf(chargeTime / maxChargeTime, 0, 1)))
+		
+		
 	if(Input.is_action_just_released("jump") and charging):
 		#apply_force(Vector2.UP.rotated(global_rotation) * 100000)
 		var dir = (global_position - bottom.global_position).normalized()
@@ -32,8 +54,13 @@ func _process(delta: float) -> void:
 		charging = false
 		onFloor = false
 		chargeTime = 0
-		pass
+		standingReturnTimePassed = 0
+		spriteLoggedDist = (sprite.global_position - global_position).length()
 		
+	if onFloor == false and charging == false:
+		var spriteInitialPos : Vector2 = global_position + (bottom.global_position - global_position).normalized() * spriteLoggedDist
+		sprite.global_position = lerp(spriteInitialPos, global_position, returnToStandingCurve.sample(clamp(standingReturnTimePassed / returnToStandingTime, 0, 1)))
+		standingReturnTimePassed += delta
 	pass
 
 
