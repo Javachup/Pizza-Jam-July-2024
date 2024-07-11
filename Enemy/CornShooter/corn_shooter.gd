4808844874b45e0476ed330410bd6cedf42a08a4
Joey Romanowski <javachup@gmail.com>
2024-07-10 20:10:58 -0400

    adds corn shooting and seed bullets

--- Enemy/CornShooter/corn_shooter.gd
+++ Enemy/CornShooter/corn_shooter.gd
@@ -1,6 +1,15 @@
 extends Node2D
 
+@export var seed_bullet:PackedScene
+
 var is_hiding := false
+
+func shoot():
+	var dir = Vector2.RIGHT
+	var bullet = seed_bullet.instantiate() as SeedBullet
+	bullet.position = position
+	bullet.direction = dir
+	get_tree().root.add_child(bullet)
 
 func hide_corn():
 	is_hiding = true
@@ -15,3 +24,7 @@ func _on_hide_area_body_entered(body):
 
 func _on_hide_area_body_exited(body):
 	reveal_corn()
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept"):
+		shoot()
