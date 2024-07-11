321134f6ee84211dd78c3a19647750b517b41013
Joey Romanowski <javachup@gmail.com>
2024-07-11 19:44:22 -0400

    updates how the melon moves

--- Enemy/MelonRoller/melon_roller.gd
+++ Enemy/MelonRoller/melon_roller.gd
@@ -1,14 +1,21 @@
-extends CharacterBody2D
+extends AnimatableBody2D
 
 @onready var health = %Health
+@onready var collision_shape_2d = $RightArea/CollisionShape2D
 
-@export var speed := 100
+@export var speed := 300
+var direction := Vector2.LEFT :
+	set(value):
+		direction = value.normalized()
 
-func _ready():
-	velocity.x = -speed
+func set_direction(to_right:bool):
+	if to_right:
+		direction = Vector2.RIGHT
+	else:
+		direction = Vector2.LEFT
 
-func _physics_process(_delta):
-	move_and_slide()
+func _physics_process(delta):
+	position += speed * delta * direction
 
 func _on_health_on_death():
 	queue_free()
