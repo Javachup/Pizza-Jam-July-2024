588ff566f3a75809f6206c6252114e9d5b52fd65
Joseph Lyons <joseph.lyons@mymail.champlain.edu>
2024-07-11 17:46:44 -0400

    Super jump done

--- Scarecrow/scarecrow_movement.gd
+++ Scarecrow/scarecrow_movement.gd
@@ -1,12 +1,19 @@
 extends RigidBody2D
 
+# States that the scarecrow can be in, mutually exclusive
+enum MoveState { IDLE, HOP_CHARGE, SUPER_JUMP_CHARGE } 
+var currentMoveState : MoveState = MoveState.IDLE
+
+@export_group("References")
 @export var bottom : RigidBody2D
 @export var groundDetectPivot : Node2D
 @export var groundDetectArea : Area2D
 
+@export_group("Idle Parameters")
 @export var idleStraightForce : float = 40000
 @export var idleStraightDamp : float = 4000
 
+@export_group("Hop Parameters")
 @export var hopRotateSpeed : float = 100
 @export var minHopForce : float = 250
 @export var maxHopForce : float = 1000
@@ -14,13 +21,20 @@ extends RigidBody2D
 @export var maxHopChargeAngle : float = 45
 @export var hopChargePowerCurve : Curve # Between 0 and 1
 
-enum MoveState { IDLE, HOP_CHARGE }
-
-var currentMoveState : MoveState = MoveState.IDLE
+@export_group("Super Jump Parameters")
+@export var superJumpMinChargeTime : float = .5
+@export var superJumpMaxChargeTime : float = 2.5
+@export var superJumpMinForce : float = 1000
+@export var superJumpMaxForce : float = 10000
+@export var superJumpForceCurve : Curve # Between 0 and 1
 
 var onGround : bool = false
 
+# Hop vars
 var hopChargeDir : int = 0
+
+# Super jump vars
+var superJumpChargeTime = 0
 
 	
 func _process(delta: float) -> void:
@@ -33,6 +47,9 @@ func _process(delta: float) -> void:
 			
 		MoveState.HOP_CHARGE:
 			hop_charge()
+			
+		MoveState.SUPER_JUMP_CHARGE:
+			super_jump_charge(delta)
 		
 	pass
 	
@@ -46,22 +63,28 @@ func _physics_process(delta: float) -> void:
 			
 		MoveState.HOP_CHARGE:
 			hop_charge_physics(delta)
+			
+		MoveState.SUPER_JUMP_CHARGE:
+			super_jump_charge_physics(delta)
 		
-	pass
-	
-	
-func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	pass
 	
 	
 func idle(delta : float):
 	
 	if onGround:
+		
+		# Check for hop input
 		# chargeDir will be -1 for left, 0 for neither / both, 1 for right
 		hopChargeDir = int(Input.is_action_just_pressed("right")) - int(Input.is_action_just_pressed("left"))
 	
 		if hopChargeDir != 0:
 			currentMoveState = MoveState.HOP_CHARGE
+			return
+			
+		# Check for super jump input
+		if Input.is_action_just_pressed("jump"):
+			currentMoveState = MoveState.SUPER_JUMP_CHARGE
 			
 	pass
 	
@@ -71,7 +94,12 @@ func idle_physics(delta : float):
 	if onGround:
 		bottom.linear_velocity.x = 0
 		bottom.angular_velocity = 0
-		bottom.rotation_degrees = 0
+		
+		#global_rotation = lerpf(global_rotation, 0, delta * 10)
+		
+		bottom.global_rotation = 0 	# I hate that I have to do this, it makes it look stiff, but this is the
+									# the only way I've found to stop a bug with the scarecrow behaving strangely
+									# when landing
 		
 	else:
 	
@@ -118,9 +146,29 @@ func calculate_hop_force():
 	return lerpf(minHopForce, maxHopForce, curveSamp)
 	
 	
-func hop(dir : Vector2, force : float):
-	apply_impulse(dir * force)
+func super_jump_charge(delta : float):
+	
+	superJumpChargeTime += delta
+	
+	if Input.is_action_just_released("jump"):
+		if superJumpChargeTime >= superJumpMinChargeTime:
+			jump(Vector2.UP, calculate_super_jump_force())
+			
+		superJumpChargeTime = 0
+		onGround = false
+		currentMoveState = MoveState.IDLE
+	
 	pass
+	
+	
+func super_jump_charge_physics(delta : float):
+	pass
+	
+	
+func calculate_super_jump_force():
+	var timePortion = (superJumpChargeTime - superJumpMinChargeTime) / (superJumpMaxChargeTime - superJumpMinChargeTime)
+	var curveSamp = superJumpForceCurve.sample(timePortion)
+	return lerpf(superJumpMinForce, superJumpMaxForce, curveSamp)
 	
 	
 func jump(dir : Vector2, force : float):
