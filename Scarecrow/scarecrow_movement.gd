e99e5990ad55ea8cbeaf4c88d37d05d33a2985b9
Joseph Lyons <joseph.lyons@mymail.champlain.edu>
2024-07-11 17:19:29 -0400

    Scarecrow hop fully featured, just might clean some more stuff up

--- Scarecrow/scarecrow_movement.gd
+++ Scarecrow/scarecrow_movement.gd
@@ -4,9 +4,15 @@ extends RigidBody2D
 @export var groundDetectPivot : Node2D
 @export var groundDetectArea : Area2D
 
+@export var idleStraightForce : float = 40000
+@export var idleStraightDamp : float = 4000
+
 @export var hopRotateSpeed : float = 100
-@export var topHopForce : float = 500
-@export var bottomHopForce : float = 750
+@export var minHopForce : float = 250
+@export var maxHopForce : float = 1000
+@export var minHopChargeAngle : float = 10
+@export var maxHopChargeAngle : float = 45
+@export var hopChargePowerCurve : Curve # Between 0 and 1
 
 enum MoveState { IDLE, HOP_CHARGE }
 
@@ -14,26 +20,12 @@ var currentMoveState : MoveState = MoveState.IDLE
 
 var onGround : bool = false
 
-var chargeDir : int = 0
+var hopChargeDir : int = 0
 
-var prevFrameBottomAngle : float = 0
-
-var wasOnGroundPrevFrame : bool = false
-
-var detectGroundBuffer : float = .2
-	
-	
 	
 func _process(delta: float) -> void:
 	
-	wasOnGroundPrevFrame = onGround
-	detectGroundBuffer -= delta
-	
-	#print(detectGroundBuffer)
-	
-	update_on_ground()
-	
-	groundDetectPivot.global_rotation = 0
+	groundDetectPivot.global_rotation = 0 # Make sure ground detect area is always facing downward
 	
 	match currentMoveState:
 		MoveState.IDLE:
@@ -59,15 +51,6 @@ func _physics_process(delta: float) -> void:
 	
 	
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	if currentMoveState == MoveState.IDLE:
-		
-		pass
-		#state.apply_torque(-bottom.global_rotation * 40000)
-		#bottom.angular_damp = (2 * PI - abs(bottom.global_rotation)) * 100
-		#if abs(bottom.global_rotation_degrees) < 1:
-			#bottom.lock_rotation = true
-	#else:
-		#bottom.lock_rotation = false
 	pass
 	
 	
@@ -75,88 +58,64 @@ func idle(delta : float):
 	
 	if onGround:
 		# chargeDir will be -1 for left, 0 for neither / both, 1 for right
-		chargeDir = int(Input.is_action_just_pressed("right")) - int(Input.is_action_just_pressed("left"))
+		hopChargeDir = int(Input.is_action_just_pressed("right")) - int(Input.is_action_just_pressed("left"))
 	
-		if chargeDir != 0:
+		if hopChargeDir != 0:
 			currentMoveState = MoveState.HOP_CHARGE
-			bottom.angular_damp = 0
 			
 	pass
 	
 	
 func idle_physics(delta : float):
 	
-	if onGround and detectGroundBuffer < 0:
-		print("here")
+	if onGround:
 		bottom.linear_velocity.x = 0
 		bottom.angular_velocity = 0
 		bottom.rotation_degrees = 0
+		
 	else:
 	
-		bottom.apply_torque(-bottom.global_rotation * 40000)
-		bottom.apply_torque(-bottom.angular_velocity * 4000)
-	
-	#bottom.angular_damp = (2 * PI - abs(bottom.global_rotation)) * 100
-	
-	
-	
-	#if onGround:
-		#bottom.angular_velocity = 0
-		
-	#bottom.angular_damp = INF
-	#
-	#apply_force(Vector2.UP * 10000)
-	#bottom.apply_force(Vector2.DOWN * 10000)
-	
-	#var prevSign = sign(prevFrameBottomAngle)
-	#var currSign = sign(bottom.global_rotation)
-	#
-	#if prevSign != currSign:
-		#bottom.global_rotation = 0
-		#bottom.angular_velocity = 0
-		#bottom.lock_rotation = true
-		#
-	#else:
-		#bottom.lock_rotation = false
-	#
-	#print(bottom.angular_damp)
-	
-	prevFrameBottomAngle = bottom.global_rotation
+		bottom.apply_torque(-bottom.global_rotation * idleStraightForce)
+		bottom.apply_torque(-bottom.angular_velocity * idleStraightDamp)
 	
 	pass
 	
 	
 func hop_charge():
 	
-	if (Input.is_action_just_released("right") and chargeDir > 0) or \
-		(Input.is_action_just_released("left") and chargeDir < 0):
+	if (Input.is_action_just_released("right") and hopChargeDir > 0) or \
+		(Input.is_action_just_released("left") and hopChargeDir < 0):
 		
 		linear_velocity = Vector2.ZERO
 		bottom.angular_velocity = 0
 		
-		#jump(Vector2.UP.rotated(bottom.global_rotation), 1000)
-		#hop(Vector2.RIGHT.rotated(bottom.global_rotation) * -chargeDir, topHopForce)
-		jump(Vector2.RIGHT.rotated(bottom.global_rotation) * -chargeDir, bottomHopForce)
+		# Make sure we actually rotated enough to launch
+		if(abs(global_rotation_degrees) >= minHopChargeAngle):		
+			jump(Vector2.RIGHT.rotated(bottom.global_rotation) * -hopChargeDir, calculate_hop_force())
+			onGround = false			
 		
-		chargeDir = 0
+		hopChargeDir = 0
 		currentMoveState = MoveState.IDLE
-		
-		detectGroundBuffer = .2
-				
+						
 	pass
 	
 	
 func hop_charge_physics(delta : float):
-	apply_force((Vector2.RIGHT * chargeDir).rotated(bottom.global_rotation) * hopRotateSpeed * delta)	
+	
+	if abs(global_rotation_degrees) >= maxHopChargeAngle:
+		angular_velocity = 0
+		linear_velocity.x = 0
+		global_rotation_degrees = clampf(global_rotation_degrees, -maxHopChargeAngle, maxHopChargeAngle)
+		
+	else:
+		apply_torque(hopChargeDir * hopRotateSpeed * delta)
+		
 	
 	
-func update_on_ground():
-	#onGround = groundDetectionRaycast.is_colliding()
-	if detectGroundBuffer > 0:
-		onGround = false
-		return
-	onGround = groundDetectArea.get_overlapping_bodies().size() != 0
-	pass
+func calculate_hop_force():
+	var rotPortion = (abs(global_rotation_degrees) - minHopChargeAngle) / (maxHopChargeAngle - minHopChargeAngle)
+	var curveSamp = hopChargePowerCurve.sample(rotPortion)
+	return lerpf(minHopForce, maxHopForce, curveSamp)
 	
 	
 func hop(dir : Vector2, force : float):
@@ -167,3 +126,12 @@ func hop(dir : Vector2, force : float):
 func jump(dir : Vector2, force : float):
 	bottom.apply_impulse(dir * force)
 	pass
+	
+
+# Since terrain will all be on terrain layer, don't need to check collision
+func _on_ground_detect_area_body_entered(body: Node2D) -> void:
+	onGround = true
+	
+	
+func _on_ground_detect_area_body_exited(body: Node2D) -> void:
+	onGround = false
