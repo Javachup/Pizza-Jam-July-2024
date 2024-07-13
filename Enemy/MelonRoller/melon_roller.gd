87c76fde856f13f385ee0145f0d75004f5a103c3
Joey Romanowski <javachup@gmail.com>
2024-07-12 20:11:16 -0400

    Fixes melon roll animation to react to the roll direction

--- Enemy/MelonRoller/melon_roller.gd
+++ Enemy/MelonRoller/melon_roller.gd
@@ -3,11 +3,13 @@ extends Area2D
 @onready var health = %Health
 @onready var right_area = %RightArea
 @onready var left_area = %LeftArea
+@onready var roll_animation_player = %RollAnimationPlayer
 
 @export var speed := 100
 var direction := Vector2.LEFT :
 	set(value):
 		direction = value.normalized()
+		roll_animation_player.speed_scale = sign(value.dot(Vector2.RIGHT))
 
 var colliding_bodies = []
 
