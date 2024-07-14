04b03589ce2771995e9df69ae3b52f6660ca1b40
Joey Romanowski <javachup@gmail.com>
2024-07-14 13:36:23 -0400

    Increases corn bullet size

--- Enemy/SeedBullet/seed_bullet.gd
+++ Enemy/SeedBullet/seed_bullet.gd
@@ -4,6 +4,8 @@ extends AnimatableBody2D
 @export var speed := 100
 @export var damage := 3
 
+@onready var sprite_2d = %Sprite2D
+const SPIN_RATE = -12
 
 var direction := Vector2.ZERO :
 	set(value):
@@ -11,3 +13,4 @@ var direction := Vector2.ZERO :
 
 func _physics_process(delta):
 	position += speed * delta * direction
+	sprite_2d.rotate(delta * SPIN_RATE)
