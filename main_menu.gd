781ed27daaa600a138e1a69fc4b6df3f47841bbb
Intern <intern@enterandromeda.com>
2024-07-14 00:46:36 -0400

    im done bruh

---
+++ main_menu.gd
@@ -0,0 +1,21 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://Prototype/Scenes/level1.tscn")
+	pass # Replace with function body.
+
+
+func _on_button_2_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
